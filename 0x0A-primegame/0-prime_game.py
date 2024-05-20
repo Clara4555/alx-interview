f919e4db741665c43f46e27386ea0e1033637373
Clara4555 <favourloyalty8@gmail.com>
2024-05-20 05:03:46 -0700

    task

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Prime game module.
+"""
+
+
+def isWinner(x, nums):
+    """Determines the winner of a prime game session
+
+    Args:
+    x (int): The number of rounds.
+    nums (list of int): A list of `x` integers representing
+    the limit for each round.
+
+    Returns:
+    str or None: The name of the player that won the most rounds.
+                 If the winner cannot be determined, returns None.
+    """
+    if x < 1 or not nums:
+        return None
+    maria_wins, ben_wins = 0, 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    for _, limit in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: limit])))
+        ben_wins += primes_count % 2 == 0
+        maria_wins += primes_count % 2 == 1
+    if maria_wins == ben_wins:
+        return None
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
